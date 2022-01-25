dfdcfd66fd8ddfdb7362912732c88239014dc085
kifen <kifenraph@gmail.com>
2022-01-25 13:30:00 +0100

    Create signature from r,s,v

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use dotenv;
+use ethers::prelude::*;
 use lib::{error::RelayServerError, relayer::Relayer};
 use std::error::Error;
 use std::fs;
-use ethers::prelude::*;
 
 #[derive(Debug)]
 struct Config {
@@ -28,9 +28,14 @@ impl Config {
 fn main() -> Result<(), Box<dyn Error>> {
     let config = Config::load_config()?;
     let provider = Relayer::<Provider<Http>>::provider(config.rpc_url)?;
-    let relayer = Relayer::new(config.private_key, provider);
-   
-    println!("{:?}", relayer);
+    let relayer = Relayer::new(config.private_key, provider)?;
 
+    let signature = Relayer::<Provider<Http>>::new_signature(
+        "0x57fffebf28f8ec6a40afa4a22ec4b681d3836ff240a6383e6d22a4c30ebb93ce",
+        "0x337957898669ca77859d560caff24a5c8f84b54ed0a1fbb102dddb5f59d74097",
+        28,
+    )
+    .unwrap();
+    println!("{}", signature);
     Ok(())
 }
--- src/relayer.rs
+++ src/relayer.rs
@@ -1,6 +1,4 @@
 use ethers::{prelude::*, utils::Ganache};
-use serde::Deserialize;
-use serde_json::Value;
 use std::error::Error;
 use url::ParseError;
 
@@ -21,10 +19,28 @@ impl<P: Middleware> Relayer<P> {
         Provider::<Http>::try_from(rpc_url)
     }
 
-    pub fn generate_typed_data(&self) -> Result<(), Box<dyn Error>> {
-        let typed_data: Value = serde_json::from_str("./typed_data.json")?;
-        println!("{}", typed_data);
-        Ok(())
+    pub fn signer(&self) -> &impl Signer {
+        self.client.signer()
+    }
+
+    pub fn verify_signature(
+        sig: &Signature,
+        address: H160,
+        message: RecoveryMessage,
+    ) -> Result<(), SignatureError> {
+        sig.verify(message, address)
+    }
+
+    pub fn new_signature(r: &str, s: &str, v: u64) -> Result<Signature, Box<dyn Error>> {
+        let r = U256::from_str_radix(r, 16)?;
+        let s = U256::from_str_radix(s, 16)?;
+
+        Ok(Signature { r, s, v })
+    }
+
+    pub fn new_recovery_message(hash_struct: &str) -> RecoveryMessage {
+        let hash_struct = hash_struct.as_bytes();
+        RecoveryMessage::Hash(H256::from_slice(hash_struct))
     }
 }
 
@@ -33,14 +49,12 @@ mod tests {
     use super::*;
 
     const PK: &str = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7";
+    const PK_ADDRESS: &str = "0x63f9725f107358c9115bc9d86c72dd5823e9b1e6";
     const BAD_PK: &str = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72";
 
     const RPC_URL: &str = "https://ropsten.infura.io/v3/c3db5ca5240b4adfbc0bd227b9f3c77a";
 
-    fn test_wallet() -> LocalWallet {
-        let ganache = Ganache::new().spawn();
-        ganache.keys()[0].clone().into()
-    }
+    const VALID_SIG: &str = "57fffebf28f8ec6a40afa4a22ec4b681d3836ff240a6383e6d22a4c30ebb93ce337957898669ca77859d560caff24a5c8f84b54ed0a1fbb102dddb5f59d740971c";
 
     fn provider(rpc_url: String) -> Result<Provider<Http>, ParseError> {
         Relayer::<Provider<Http>>::provider(rpc_url)
@@ -58,5 +72,24 @@ mod tests {
     #[test]
     fn it_errors_creating_relayer_with_invalid_pk() {
         assert_eq!(relayer(BAD_PK.to_string()).is_err(), true);
+    }
+
+    #[test]
+    fn it_returns_signer() {
+        let relayer = relayer(PK.to_string()).unwrap();
+        let address = H160::from_slice(PK_ADDRESS.as_bytes());
+        assert_eq!(relayer.signer().address(), address);
+    }
+
+    #[test]
+    fn it_verifies_signature() {
+        let signature = Relayer::<Provider<Http>>::new_signature(
+            "0x57fffebf28f8ec6a40afa4a22ec4b681d3836ff240a6383e6d22a4c30ebb93ce",
+            "0x337957898669ca77859d560caff24a5c8f84b54ed0a1fbb102dddb5f59d74097",
+            28,
+        )
+        .unwrap();
+
+        assert_eq!(VALID_SIG.to_string(), signature.to_string());
     }
 }
